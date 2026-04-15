93206c0e01c531f79077163c7008e40530f2ac25
sa2oury <abdallahsakoury8@gmail.com>
2026-04-15 14:32:06 +0200

    Update run_test.py

--- AI/SRC/run_test.py
+++ AI/SRC/run_test.py
@@ -4,10 +4,10 @@ from test import test_model
 if __name__ == '__main__':
     accuracy = test_model(
         model_name='efficientnet_b0',
-        weights_path='checkpoints/efficientnet_b0_best.pth',
+        weights_path='checkpoints\efficientnet_b0_best.pth',
         data_root=r'AI\Data',
         batch_size=8,
         num_workers=2,
     )
-
+    print("testy testy")
     print(f'Returned accuracy: {accuracy:.4f}')
